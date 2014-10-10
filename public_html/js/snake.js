284c82398b5be4abdf15f49462541420e3d48175
katalina arce <katalinaarce34@gmail.com>
2014-10-10 09:11:43 -0700

    snake progress

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,75 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
+var snakeLength;
+var snakeSize;
 
+var food;
+
+var context;
+var screenWidth;
+var screanHeight;
+
+
+gameInitialize();
+snakeInitialize();
+setInterval(gameLoop, 1000/20);
+
+function gameInitialize () {
+   var canvas = document.getElementById ("game-screen"); 
+context = canvas.getContext("2d");
+
+screenWidth = window.innerWidth;
+screenHeight = window.innerHeight;
+
+canvas.width = screenWidth;
+canvas.height = screenHeight;
+}
+
+function gameLoop(){
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    
+}
+   
+
+function gameDraw() {
+  context.fillStyle = "rgb(0, 30, 255)";
+  context.fillRect(0, 0, screenWidth, screenHeight);
+
+}
+
+function snakeInitialize(){
+    snake = [];
+    snakeLength = 5;
+    snakeSize = 20;
+    
+    for( var index = snakeLength - 1; index >= 0; index--){
+      snake.push({
+          x: index,
+          y: 0
+      });
+      
+      
+    }
+}
+
+function snakeDraw(){
+    for(var index = 0; index < snake.length; index++){
+      context.fillStyle = "white";
+      context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+  
+    }
+}
+
+function snakeUpdate(){
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    snakeHeadX++;
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
 
